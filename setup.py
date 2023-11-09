3f4793df47b1640860668f9813190c1f7abaf9ba
JinalParikh25 <theparikh303@gmail.com>
2023-11-09 15:42:32 +0100

    commit setup.py file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='DSSS-Exe',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/JinalParikh25/githubtest.git',
+    license='',
+    author='HP',
+
+
+    author_email='theparikh303@gmail.com',
+    description='analytics and reports'
+)
